d89bd2bb065a1177dcd891b685613ee33e4b9d5f
Chris Hakos <chrishakos@gmail.com>
2017-12-08 13:57:35 -0500

    write template rendering logic

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
